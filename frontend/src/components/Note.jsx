38c309e93814288220f78cdde3f0cddeb8e76344
Howard <howard.felix@outlook.com>
2024-06-30 15:35:00 +0700

    uibuild

--- frontend/src/components/Note.jsx
+++ frontend/src/components/Note.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Notification from './Notification'; // Adjust the path if necessary
 
 const Names = ({ person, deleteName }) => {
   const [showAdditionalSection, setShowAdditionalSection] = useState(false);
@@ -21,97 +22,66 @@ const Names = ({ person, deleteName }) => {
       </div>
       {showAdditionalSection && (
         <div style={{ marginTop: '1rem', padding: '1rem', backgroundColor: '#f0f0f0' }}>
-          {/* This is the blank section for future use */}
           <h4>Additional Details</h4>
           <p>This section can be used to add more information about {person.name}.</p>
-          {/* You can add form fields, text inputs, or any other content here for future use */}
         </div>
       )}
     </div>
   );
 };
-  
-const Filter = ({ searchName, handleSearchName }) => {
-    return (
-      <div>
-        SEARCH:<br/> {' '}
-        <input id="searchInput" name="search" value={searchName} onChange={handleSearchName} autoComplete="off" />
-      </div>
-    );
-  }  
-  
-  const PersonForm = ({ addName, newName, handleNameChange, newNumber, handleNumberChange }) => {
-    const [nameError, setNameError] = useState('');
-    const [passportError, setPassportError] = useState('');
-  
-    const handleSubmit = (event) => {
-      event.preventDefault();
-  
-      if (!newName.trim() && !newNumber.trim()) {
-        setNameError('Name and passport number are required.');
-        setPassportError('');
-      } else if (!newName.trim()) {
-        setNameError('Name is required.');
-        setPassportError('');
-      } else if (!newNumber.trim()) {
-        setPassportError('Passport number is required.');
-        setNameError('');
-      } else {
-        setNameError('');
-        setPassportError('');
-        addName(event);
-      }
 
-      setTimeout(() => {
-        setNameError('');
-        setPassportError('');
-      }, 4000);
-    };
-  
-    return (
-      <div>
-        <form onSubmit={handleSubmit}>
-          <div style={{ marginBottom: '10px' }}>
-            Name:<br/> 
-            <input 
-              id="nameInput" 
-              name="name" 
-              value={newName} 
-              onChange={handleNameChange} 
-              autoComplete="off" 
-            />
-            {nameError && <span style={{ color: 'red' }}>{nameError}</span>}
-          </div>
-          <div style={{ marginBottom: '10px' }}>
-            Passport Number:<br/> 
-            <input 
-              id="passportNumberInput" 
-              name="passportNumber" 
-              value={newNumber} 
-              onChange={handleNumberChange} 
-              autoComplete="off" 
-            />
-            {passportError && <span style={{ color: 'red' }}>{passportError}</span>}
-          </div>
-          <div>
-            <button type="submit">Submit</button>
-          </div>
-        </form>
-      </div>
-    );
-  };  
-  
-  const Persons = ({ persons, deleteName }) => {
-    return (
-      <div>
-        {persons.map((person) => {
-          return (
-            <Names key={person.name} person={person} deleteName={deleteName} />
-          )
-        })}
-      </div>
-    )
-  }
-  
-  export { Names, Filter, PersonForm, Persons };
-  
+const Filter = ({ searchName, handleSearchName }) => {
+  return (
+    <div>
+      SEARCH:<br/> {' '}
+      <input id="searchInput" name="search" value={searchName} onChange={handleSearchName} autoComplete="off" />
+    </div>
+  );
+};
+
+const PersonForm = ({ addName, newName, handleNameChange, newNumber, handleNumberChange, successMessage, errorMessage }) => {
+  return (
+    <div>
+      <form onSubmit={addName}>
+        <div style={{ marginBottom: '10px' }}>
+          Name:<br/> 
+          <input 
+            id="nameInput" 
+            name="name" 
+            value={newName} 
+            onChange={handleNameChange} 
+            autoComplete="off" 
+          />
+        </div>
+        <div style={{ marginBottom: '10px' }}>
+          Passport Number:<br/> 
+          <input 
+            id="passportNumberInput" 
+            name="passportNumber" 
+            value={newNumber} 
+            onChange={handleNumberChange} 
+            autoComplete="off" 
+          />
+        </div>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+      <Notification successMessage={successMessage} errorMessage={errorMessage} />
+    </div>
+  );
+};
+
+const Persons = ({ persons, deleteName }) => {
+  return (
+    <div>
+      {persons.map((person) => {
+        return (
+          <Names key={person.name} person={person} deleteName={deleteName} />
+        )
+      })}
+    </div>
+  )
+}
+
+export { Names, Filter, PersonForm, Persons };
