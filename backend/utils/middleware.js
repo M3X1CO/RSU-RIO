b3d91fb88d06218c55c90e45e34a9bf8ab6128cb
Howard <howard.felix@outlook.com>
2024-07-18 13:00:59 +0700

    uibuild

--- backend/utils/middleware.js
+++ backend/utils/middleware.js
@@ -1,43 +1,41 @@
-const logger = require('./logger')
+const logger = require('./logger');
 
 const requestLogger = (request, response, next) => {
-  logger.info('Method:', request.method)
-  logger.info('Path:  ', request.path)
-  logger.info('Body:  ', request.body)
-  logger.info('---')
-  next()
-}
+  logger.info('Method:', request.method);
+  logger.info('Path:  ', request.path);
+  logger.info('Body:  ', request.body);
+  logger.info('---');
+  next();
+};
 
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
+  response.status(404).send({ error: 'unknown endpoint' });
+};
 
 const errorHandler = (error, request, response, next) => {
-  console.log('error:', error)
-
   if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
+    logger.error('Error: Malformatted id:', error.message);
+    return response.status(400).send({ error: 'Malformatted id' });
   } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
+    logger.error('Validation Error:', error.message);
+    return response.status(400).json({ error: error.message });
   } else if (error.name === 'MongoServerError' && error.message.includes('E11000 duplicate key error')) {
-    return response.status(400).json({
-      error: 'expected `username` to be unique'
-    })
+    logger.error('MongoDB Duplicate Key Error:', error.message);
+    return response.status(400).json({ error: 'Expected `username` to be unique' });
   } else if (error.name === 'JsonWebTokenError') {
-    return response.status(401).json({
-      error: 'invalid token'
-    })
+    logger.error('JWT Error:', error.message);
+    return response.status(401).json({ error: 'Invalid token' });
   } else if (error.name === 'TokenExpiredError') {
-    return response.status(401).json({
-      error: 'token expired'
-    })
+    logger.error('JWT Token Expired:', error.message);
+    return response.status(401).json({ error: 'Token expired' });
   }
 
-  next(error)
-}
+  logger.error('Internal Server Error:', error.message);
+  next(error);
+};
 
 module.exports = {
   requestLogger,
   unknownEndpoint,
-  errorHandler
-}
+  errorHandler,
+};
