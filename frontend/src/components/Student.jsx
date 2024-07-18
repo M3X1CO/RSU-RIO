798a9e3b1b4d3ea7a4c32e8b8a455af82e2f9f62
Howard <howard.felix@outlook.com>
2024-07-18 14:13:18 +0700

    uibuild

--- frontend/src/components/Student.jsx
+++ frontend/src/components/Student.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Student = ({ name, passport }) => {
+  const [showDetails, setShowDetails] = useState(false)
+
+  const toggleDetails = () => {
+    setShowDetails(!showDetails)
+  }
+
   return (
     <div>
       <p>{name} {passport}</p>
+      <button onClick={toggleDetails}>
+        {showDetails ? 'Hide Details' : 'Show Details'}
+      </button>
+      {showDetails && (
+        <div>
+          {/* Add additional details here */}
+          <p>Additional details about the student</p>
+        </div>
+      )}
     </div>
   )
 }
