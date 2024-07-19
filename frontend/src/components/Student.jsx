b539842d46ae0935daa8e048bb0a2673710e48a6
Howard <howard.felix@outlook.com>
2024-07-19 14:47:54 +0700

    uibuild

--- frontend/src/components/Student.jsx
+++ frontend/src/components/Student.jsx
@@ -1,15 +1,13 @@
 import React, { useState } from 'react'
 
-const Student = ({ name, passport, id, user, handleDelete }) => {
+const Student = ({ student, handleDelete }) => {
   const [showDetails, setShowDetails] = useState(false)
 
-  const toggleDetails = () => {
-    setShowDetails(!showDetails)
-  }
+  const toggleDetails = () => setShowDetails(!showDetails)
 
   const confirmDelete = () => {
-    if (window.confirm(`Are you sure you want to delete ${name}?`)) {
-      handleDelete(id)
+    if (window.confirm(`Are you sure you want to delete ${student.name}?`)) {
+      handleDelete(student.id)
     }
   }
 
@@ -17,9 +15,9 @@ const Student = ({ name, passport, id, user, handleDelete }) => {
     <li className='student'>
       <p>
         <strong>Student's Name: </strong> 
-        <span style={{ color: 'green' }}>{name}<br/></span> 
+        <span style={{ color: 'green' }}>{student.name}<br/></span> 
         <strong>Student's Passport Number: </strong> 
-        <span style={{ color: 'green' }}>{passport}</span>
+        <span style={{ color: 'green' }}>{student.passport}</span>
       </p>
       <button onClick={toggleDetails}>
         {showDetails ? 'Hide Details' : 'Show Details'}
@@ -27,7 +25,7 @@ const Student = ({ name, passport, id, user, handleDelete }) => {
       {showDetails && (
         <div>
           <p>Additional details about the student</p>
-          <p>Created by: {user.name}</p>
+          <p>Created by: {student.user.name}</p>
         </div>
       )}
       <button onClick={confirmDelete} className="delete-button">Delete</button>
