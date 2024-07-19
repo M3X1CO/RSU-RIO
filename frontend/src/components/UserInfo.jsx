3bc4bf002fa184f8865b6c62bc76eca714a342a1
Howard <howard.felix@outlook.com>
2024-07-19 16:16:19 +0700

    uibuild

--- frontend/src/components/UserInfo.jsx
+++ frontend/src/components/UserInfo.jsx
@@ -4,7 +4,7 @@ const UserInfo = ({ user }) => {
   return (
     <div className="user-info">
       <span className="icon">👤</span> {/* Optional: Add an icon if desired */}
-      <p>{user.name} logged in</p>
+      <p>{user.name}</p>
     </div>
   )
 }
