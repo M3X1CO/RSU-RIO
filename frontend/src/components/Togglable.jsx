f0f1830e76d0f62a5d08d813563f7c4749ecde0f
Howard <howard.felix@outlook.com>
2024-07-19 15:33:24 +0700

    uibuild

--- frontend/src/components/Togglable.jsx
+++ frontend/src/components/Togglable.jsx
@@ -25,7 +25,7 @@ const Togglable = forwardRef((props, ref) => {
       <div style={showWhenVisible} className="togglableContent">
         {props.children}
         <div className="centered-button-container">
-          <button type="button" onClick={cancelForm} className="cancel-button">Cancel</button>
+          <button type="button" onClick={toggleVisibility} className="cancel-button">Cancel</button>
         </div>
       </div>
     </div>
