329c0f5adaa8cbdbe961c3e8c3c9eb4ada18e2b4
Howard <howard.felix@outlook.com>
2024-07-24 15:30:28 +0700

    uibuild

--- backend/controllers/students.js
+++ backend/controllers/students.js
@@ -56,7 +56,7 @@ studentRouter.post('/', async (request, response) => {
 })
 
 studentRouter.get('/:id', async (request, response) => {
-  const student = await Student.findById(request.params.id).populate('student', { username: 1, name: 1 })
+  const student = await Student.findById(request.params.id).populate('student', { name: 1, passport: 1 })
   if (student) {
     response.json(student)
   } else {
