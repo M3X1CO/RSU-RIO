5ea08f26358d9de4b70eca83a1d80afa35f0b740
Howard <howard.felix@outlook.com>
2024-08-01 11:04:11 +0700

    frontendBuild

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,135 +1,23 @@
-import React, { useState, useEffect, useRef } from 'react'
-import Student from './components/Student'
-import Notification from './components/Notification'
-import Footer from './components/Footer'
-import studentsService from './services/students'
-import loginService from './services/login'
-import LoginForm from './components/LoginForm'
-import Togglable from './components/Togglable'
-import StudentForm from './components/StudentForm'
-import UserInfo from './components/UserInfo'
+import { useState } from 'react'
 
-const App = () => {
-  const [students, setStudents] = useState([])
-  const [errorMessage, setErrorMessage] = useState(null)
-  const [user, setUser] = useState(null)
-  const [loginVisible, setLoginVisible] = useState(false)
-
-  const studentFormRef = useRef()
-
-  useEffect(() => {
-    const loggedUserJSON = window.localStorage.getItem('loggedStudentappUser')
-    if (loggedUserJSON) {
-      const user = JSON.parse(loggedUserJSON)
-      setUser(user)
-      studentsService.setToken(user.token)
-    }
-  }, [])
-
-  useEffect(() => {
-    studentsService.getAll()
-      .then(initialStudents => {
-        setStudents(initialStudents)
-      })
-      .catch(error => {
-        console.error('Error fetching students:', error)
-        setErrorMessage('Failed to fetch students')
-        setTimeout(() => {
-          setErrorMessage(null)
-        }, 5000)
-      })
-  }, [user])
-
-  const handleLogin = async (username, password) => {
-    try {
-      const user = await loginService.login({ username, password })
-      window.localStorage.setItem('loggedStudentappUser', JSON.stringify(user))
-      studentsService.setToken(user.token)
-      setUser(user)
-    } catch (error) {
-      setErrorMessage('Wrong credentials')
-      setTimeout(() => {
-        setErrorMessage(null)
-      }, 5000)
-    }
-  }
-
-  const addStudent = (studentObject) => {
-    studentFormRef.current.toggleVisibility()
-    studentsService
-      .create(studentObject)
-      .then(returnedStudent => {
-        setStudents([...students, returnedStudent])
-      })
-      .catch(error => {
-        console.error('Error adding student:', error)
-        setErrorMessage('Failed to add student')
-        setTimeout(() => {
-          setErrorMessage(null)
-        }, 5000)
-      })
-  }
-
-  const deleteStudent = async (id) => {
-    if (window.confirm('Are you sure you want to delete this student?')) {
-      try {
-        await studentsService.remove(id)
-        setStudents(students.filter(student => student.id !== id))
-      } catch (error) {
-        console.error('Error deleting student:', error)
-        setErrorMessage('Failed to delete student')
-        setTimeout(() => {
-          setErrorMessage(null)
-        }, 5000)
-      }
-    }
-  }  
-
-  const loginForm = () => {
-    const hideWhenVisible = { display: loginVisible ? 'none' : '' }
-    const showWhenVisible = { display: loginVisible ? '' : 'none' }
-
-    return (
-      <div className="form-container">
-        <div style={hideWhenVisible}>
-          <button onClick={() => setLoginVisible(true)} className="login-button">Log In</button>
-        </div>
-        <div style={showWhenVisible} className="togglableContent">
-          <LoginForm handleSubmit={handleLogin} />
-          <div className="centered-button-container">
-            <button onClick={() => setLoginVisible(false)} className="cancel-button">Cancel</button>
-          </div>
-        </div>
-      </div>
-    )
-  }
+function App() {
+  const [count, setCount] = useState(0)
 
   return (
-    <div>
-      <h1>Students</h1>
-
-      <Notification message={errorMessage} />
-
-      {!user && loginForm()}
-      {user && 
-      <div>
-        <UserInfo user={user} />
-
-        <Togglable buttonLabel='New Student' ref={studentFormRef}>
-          <StudentForm createStudent={addStudent} />
-        </Togglable>
-      </div>}
-      <ul className="student-list">
-        {students.map(student => (
-          <Student 
-            key={student.id}
-            student={student}
-            handleDelete={deleteStudent}
-          />
-        ))}
-      </ul>
-      <Footer />
-    </div>
+    <>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
 }
 
