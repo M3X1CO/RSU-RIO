a62f6a261161a78afefc1cc5b95fe6cd4f69bc34
Howard <howard.felix@outlook.com>
2024-08-07 12:32:39 +0700

    uibuild

--- frontend/src/components/StudentDetails.jsx
+++ frontend/src/components/StudentDetails.jsx
@@ -5,68 +5,44 @@ const ITEMS_PER_PAGE = 15;
 const StudentDetails = ({ student }) => {
   const [currentPage, setCurrentPage] = useState(1);
 
+  const studentEntries = Object.entries(student).filter(([key, value]) => 
+    typeof value !== 'object' && typeof value !== 'function'
+  );
+
+  const totalItems = studentEntries.length;
+  const pageCount = Math.ceil(totalItems / ITEMS_PER_PAGE);
+
+  const start = (currentPage - 1) * ITEMS_PER_PAGE;
+  const end = start + ITEMS_PER_PAGE;
+  const currentFields = studentEntries.slice(start, end);
+
   return (
     <div className="student-details">
       <h3>Student Details</h3>
-      <PaginatedStudentFields student={student} itemsPerPage={ITEMS_PER_PAGE} currentPage={currentPage} />
-      <Pagination 
-        totalItems={Object.keys(student).length}
-        itemsPerPage={ITEMS_PER_PAGE}
-        currentPage={currentPage}
-        onPageChange={setCurrentPage}
-      />
+      <table className="student-fields">
+        <tbody>
+          {currentFields.map(([key, value]) => (
+            <tr key={key}>
+              <td>{key}:</td>
+              <td>{String(value)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      {pageCount > 1 && (
+        <div className="pagination">
+          {Array.from({ length: pageCount }, (_, i) => i + 1).map(page => (
+            <button
+              key={page}
+              onClick={() => setCurrentPage(page)}
+              className={currentPage === page ? 'active' : ''}
+            >
+              {page}
+            </button>
+          ))}
+        </div>
+      )}
     </div>
-  );
-};
-
-const PaginatedStudentFields = ({ student, itemsPerPage, currentPage }) => {
-  const start = (currentPage - 1) * itemsPerPage;
-  const end = start + itemsPerPage;
-  const currentFields = Object.entries(student).slice(start, end);
-
-  return (
-    <table className="student-fields">
-      <tbody>
-        {currentFields.map(([key, value]) => renderDetailRow(key, value, key, null, false))}
-      </tbody>
-    </table>
-  );
-};
-
-const Pagination = ({ totalItems, itemsPerPage, currentPage, onPageChange }) => {
-  const pageCount = Math.ceil(totalItems / itemsPerPage);
-
-  return (
-    <div className="pagination">
-      {Array.from({ length: pageCount }, (_, i) => i + 1).map(page => (
-        <button
-          key={page}
-          onClick={() => onPageChange(page)}
-          className={currentPage === page ? 'active' : ''}
-        >
-          {page}
-        </button>
-      ))}
-    </div>
-  );
-};
-
-const renderDetailRow = (label, value, key, handleInputChange, isEditable) => {
-  return (
-    <tr key={key}>
-      <td>{label}:</td>
-      <td>
-        {isEditable ? (
-          <input
-            type="text"
-            value={value}
-            onChange={(e) => handleInputChange(key, e.target.value)}
-          />
-        ) : (
-          value
-        )}
-      </td>
-    </tr>
   );
 };
 
