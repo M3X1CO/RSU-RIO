b5e92293d99375e7606c81f284b1336e5803c90c
Howard <howard.felix@outlook.com>
2024-08-08 15:40:20 +0700

    uibuild

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
-import Header from './components/Header';
-import Notification from './components/Notification';
-import LoginFormWrapper from './components/LoginFormWrapper';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
+import React, { useRef, useState } from 'react'
+import Notification from './components/Notification'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import LoginFormWrapper from './components/LoginFormWrapper'
+import MainContent from './components/MainContent'
+import useStudents from './hooks/useStudents'
+import useAuth from './hooks/useAuth'
 
 const App = () => {
+  const { user, errorMessage: authError, login, logout } = useAuth()
+  const { 
+    students, 
+    errorMessage: studentError, 
+    addStudent, 
+    deleteStudent, 
+    updateStudent 
+  } = useStudents(user)
+  const studentFormRef = useRef()
+  const [oldPassportSearch, setOldPassportSearch] = useState('')
+  const [newPassportSearch, setNewPassportSearch] = useState('')
+
+  const errorMessage = authError || studentError
+
+  const filteredStudents = students.filter(student =>
+    student.oldPassportNumber.toLowerCase().includes(oldPassportSearch.toLowerCase()) &&
+    student.newPassportNumber.toLowerCase().includes(newPassportSearch.toLowerCase())
+  )
+
   return (
     <div className="app-container">
       <Header />
@@ -34,4 +55,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default App
