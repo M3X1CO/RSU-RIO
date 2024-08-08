cc21259e4972bc1da0d97aa34d07318bf6f8f528
Howard <howard.felix@outlook.com>
2024-08-08 12:23:43 +0700

    uibuild

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,37 +2,14 @@ import React from 'react'
 import UserInfo from './UserInfo'
 
 const Header = ({ user, logout }) => {
-  const headerStyle = {
-    color: '#979694',
-    fontStyle: 'italic',
-    fontSize: '24px',
-    textAlign: 'center',
-    position: 'relative',
-    padding: '10px',
-  }
-
-  const userInfoContainerStyle = {
-    position: 'absolute',
-    top: '10px',
-    right: '10px',
-    textAlign: 'right',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'flex-end',
-  }
-
-  const userInfoStyle = {
-    marginBottom: '5px',
-  }
-
   return (
-    <div>
-      <div style={headerStyle}>
+    <div className="header-container">
+      <div className="header-title">
         <strong><h1>Student Management App</h1></strong>
       </div>
       {user && (
-        <div style={userInfoContainerStyle}>
-          <div style={userInfoStyle}>
+        <div className="user-info-container">
+          <div className="user-info">
             <UserInfo user={user} />
           </div>
           <button onClick={logout} className="logout-button">Logout</button>
