ead2d54fcf51c1d041f7af45fe6f9ff286c76037
Howard <howard.felix@outlook.com>
2024-08-08 15:38:12 +0700

    uibuild

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,55 +1,37 @@
-import React, { useRef, useState } from 'react'
-import Notification from './components/Notification'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import LoginFormWrapper from './components/LoginFormWrapper'
-import MainContent from './components/MainContent'
-import useStudents from './hooks/useStudents'
-import useAuth from './hooks/useAuth'
+import React from 'react';
+import Header from './components/Header';
+import Notification from './components/Notification';
+import LoginFormWrapper from './components/LoginFormWrapper';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 
 const App = () => {
-  const { user, errorMessage: authError, login, logout } = useAuth()
-  const { 
-    students, 
-    errorMessage: studentError, 
-    addStudent, 
-    deleteStudent, 
-    updateStudent 
-  } = useStudents(user)
-  const studentFormRef = useRef()
-  const [oldPassportSearch, setOldPassportSearch] = useState('')
-  const [newPassportSearch, setNewPassportSearch] = useState('')
-
-  const errorMessage = authError || studentError
-
-  const filteredStudents = students.filter(student =>
-    student.oldPassportNumber.toLowerCase().includes(oldPassportSearch.toLowerCase()) &&
-    student.newPassportNumber.toLowerCase().includes(newPassportSearch.toLowerCase())
-  )
-
   return (
-    <div>
+    <div className="app-container">
       <Header />
       <Notification message={errorMessage} />
 
-      {!user && <LoginFormWrapper handleLogin={login} />}
-      {user && 
-        <MainContent 
-          user={user}
-          studentFormRef={studentFormRef}
-          addStudent={addStudent}
-          students={filteredStudents}
-          deleteStudent={deleteStudent}
-          updateStudent={updateStudent}
-          oldPassportSearch={oldPassportSearch}
-          newPassportSearch={newPassportSearch}
-          setOldPassportSearch={setOldPassportSearch}
-          setNewPassportSearch={setNewPassportSearch}
-        />
-      }
+      <main className="main-content">
+        {!user && <LoginFormWrapper handleLogin={login} />}
+        {user && (
+          <MainContent
+            user={user}
+            studentFormRef={studentFormRef}
+            addStudent={addStudent}
+            students={filteredStudents}
+            deleteStudent={deleteStudent}
+            updateStudent={updateStudent}
+            oldPassportSearch={oldPassportSearch}
+            newPassportSearch={newPassportSearch}
+            setOldPassportSearch={setOldPassportSearch}
+            setNewPassportSearch={setNewPassportSearch}
+          />
+        )}
+      </main>
+
       <Footer addStudent={addStudent} user={user} logout={logout} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
