d61334f2cfaeaed0253abaa631b6879a24955bb2
Howard <howard.felix@outlook.com>
2024-08-08 13:50:13 +0700

    uibuild

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 
 const Search = ({ oldPassportSearch, newPassportSearch, setOldPassportSearch, setNewPassportSearch }) => {
   return (
-    <div>
+    <div className="search-container">
       <input
+        className="search-input"
         type="text"
         placeholder="Search Old Passport Number"
         value={oldPassportSearch}
         onChange={(e) => setOldPassportSearch(e.target.value)}
       />
       <input
+        className="search-input"
         type="text"
         placeholder="Search New Passport Number"
         value={newPassportSearch}
