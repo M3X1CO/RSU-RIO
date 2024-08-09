7ea77e6fd181b7482f7403d90d164041658c0a68
Howard <howard.felix@outlook.com>
2024-08-09 14:29:23 +0700

    uibuild

--- frontend/src/components/RegisterUser.jsx
+++ frontend/src/components/RegisterUser.jsx
@@ -54,16 +54,14 @@ const RegisterUser = ({ user, handleRegister, setView }) => {
             required
           />
         </div>
-        {!user && (
-          <div className="register-button-container">
-            <button type="submit">Register</button>
-            <button onClick={() => setView('main')}>Home</button>
-          </div>
-        )}
+        <div className="register-button-container">
+          <button type="submit">Register</button>
+          {user && <button onClick={() => setView('main')}>Home</button>}
+        </div>
       </form>
     </div>
   );
-};
+}  
 
 RegisterUser.propTypes = {
   handleRegister: PropTypes.func.isRequired,
