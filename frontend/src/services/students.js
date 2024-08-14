1356e6e18955a3885ad3699ca7e5f42f44a0f3bf
Howard <howard.felix@outlook.com>
2024-08-14 12:28:43 +0700

    uibuild

--- frontend/src/services/students.js
+++ frontend/src/services/students.js
@@ -1,42 +1,62 @@
 import axios from 'axios'
+
 const baseUrl = '/api/students'
 
-let token = null
+const api = axios.create({
+  baseURL: baseUrl,
+})
 
-const setToken = newToken => {
-  token = `Bearer ${newToken}`
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('loggedStudentappUser')
+    if (token) {
+      const user = JSON.parse(token)
+      config.headers['Authorization'] = `Bearer ${user.token}`
+    }
+    return config
+  },
+  (error) => Promise.reject(error)
+)
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      localStorage.removeItem('loggedStudentappUser')
+      window.location = '/login'
+    }
+    return Promise.reject(error)
+  }
+)
+
+const verifyToken = async () => {
+  try {
+    const response = await api.get('/verify-token')
+    return response.data
+  } catch (error) {
+    console.error('Token verification failed:', error)
+    throw error
+  }
 }
 
 const getAll = async () => {
-  const config = {
-    headers: { Authorization: token },
-  }
-  const response = await axios.get(baseUrl, config)
+  const response = await api.get('/')
   return response.data
 }
 
-const create = async newObject => {
-  const config = {
-    headers: { Authorization: token },
-  }
-  const response = await axios.post(baseUrl, newObject, config)
+const create = async (newObject) => {
+  const response = await api.post('/', newObject)
   return response.data
 }
 
 const update = async (id, newObject) => {
-  const config = {
-    headers: { Authorization: token },
-  }
-  const response = await axios.put(`${baseUrl}/${id}`, newObject, config)
+  const response = await api.put(`/${id}`, newObject)
   return response.data
 }
 
-const remove = async id => {
-  const config = {
-    headers: { Authorization: token },
-  }
-  const response = await axios.delete(`${baseUrl}/${id}`, config)
+const remove = async (id) => {
+  const response = await api.delete(`/${id}`)
   return response.data
 }
 
-export default { getAll, create, update, remove, setToken }
+export default { getAll, create, update, remove, verifyToken }
